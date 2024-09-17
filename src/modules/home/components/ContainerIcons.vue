018913a579ad7e394ad59750ec86e25653233d91
Luiz Carvalho <luizottavio49@gmail.com>
2024-09-17 19:45:17 -0300

    icons example

--- src/modules/home/components/ContainerIcons.vue
+++ src/modules/home/components/ContainerIcons.vue
@@ -1,7 +1,34 @@
 <template>
-  <DefaultContainer title="Icons" />
+  <DefaultContainer title="Icons" description="Icons with Phosphor library">
+    <div class="flex items-center justify-center gap-4">
+      <IconExampleCard :icon="PhArrowsCounterClockwise" />
+      <IconExampleCard :icon="PhBackspace" />
+      <IconExampleCard :icon="PhBellSimpleRinging" />
+      <IconExampleCard :icon="PhCheckCircle" />
+      <IconExampleCard :icon="PhFileVue" />
+      <IconExampleCard :icon="PhFolderOpen" />
+      <IconExampleCard :icon="PhHouseLine" />
+      <IconExampleCard :icon="PhMicrophoneSlash" />
+      <IconExampleCard :icon="PhPaperPlaneTilt" />
+      <IconExampleCard :icon="PhUserCircle" />
+    </div>
+  </DefaultContainer>
 </template>
 
 <script setup lang="ts">
 import DefaultContainer from '@/components/DefaultContainer.vue'
+import IconExampleCard from './IconExampleCard.vue'
+
+import {
+  PhArrowsCounterClockwise,
+  PhBackspace,
+  PhBellSimpleRinging,
+  PhCheckCircle,
+  PhFileVue,
+  PhFolderOpen,
+  PhHouseLine,
+  PhMicrophoneSlash,
+  PhPaperPlaneTilt,
+  PhUserCircle
+} from '@phosphor-icons/vue'
 </script>
---
+++ src/modules/home/components/IconExampleCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="flex h-14 w-14 items-center justify-center gap-2 rounded-full border border-primary-500/20 shadow-sm"
+  >
+    <component :is="icon" class="h-6 w-6 text-primary-500" weight="bold" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: Object,
+    required: true
+  }
+})
+</script>
